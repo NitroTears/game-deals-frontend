cd347e8bd9760d0f7e2fdf62644f2715231a4168
Luke Kellett <lkellett2@outlook.com>
2022-05-02 10:00:53 +1000

    Setup ChakraUI and ES Lint

--- resources/js/components/dealshome.js
+++ resources/js/components/dealshome.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
+
 const DealsHome = () => {
     return (
-        <div className="container mt-5">
+        <div className="mt-5">
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card text-center">
--- resources/js/index.js
+++ resources/js/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import DealsHome from './components/dealshome';
+import React from "react";
+import ReactDOM from "react-dom";
+import DealsHome from "./components/dealshome";
+import { ChakraProvider } from "@chakra-ui/react";
+
 function Index() {
     return (
-        <div>
+        <ChakraProvider>
             <DealsHome />
-        </div>
+        </ChakraProvider>
     );
 }
 
 export default Index;
 
-if (document.getElementById('app')) {
-    ReactDOM.render(<Index />, document.getElementById('app'));
+if (document.getElementById("app")) {
+    ReactDOM.render(<Index />, document.getElementById("app"));
 }
