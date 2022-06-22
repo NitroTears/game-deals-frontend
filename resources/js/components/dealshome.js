a309ceb3d12095e41e7d785495eedcbd68448456
Luke Kellett <lkellett2@outlook.com>
2022-06-22 20:29:52 +1000

    Added selects for platform/physicality, search bar and button

--- resources/js/components/dealshome.js
+++ resources/js/components/dealshome.js
@@ -1,4 +1,13 @@
-import { Container, useToast, Spinner, Center } from "@chakra-ui/react";
+import {
+  Container,
+  useToast,
+  Spinner,
+  Center,
+  HStack,
+  Select,
+  Input,
+  Button,
+} from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import NewNavLink from "./navbar/index";
 import DealsTable from "./deals-table/dealsTable";
@@ -7,6 +16,9 @@ import axios from "axios";
 const DealsHome = (props) => {
   const [dealsData, setDealsData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [selectedPlatform, setSelectedPlatform] = useState(null);
+  const [selectedPhysical, setSelectedPhysical] = useState(null);
+  const [searchName, setSearchName] = useState(null);
   const toast = useToast();
 
   // Get the deals from the API.
@@ -33,21 +45,60 @@ const DealsHome = (props) => {
         });
     }
   }, [dealsData]);
-  
+
+  const searchDeals = () => {
+    //Use search constraints to get deals from backend.
+  };
+
   return (
     <>
       <NewNavLink {...props} />
       <Container maxW={"container.xl"}>
         {loading ? (
-         <Center height={"20rem"}>
-           <Spinner speed="0.8s" color="blue.300" size="xl" />
-         </Center>
+          <Center height={"20rem"}>
+            <Spinner speed="0.8s" color="blue.300" size="xl" />
+          </Center>
         ) : (
           <>
+            <br />
+            <HStack paddingBottom={"0.5rem"}>
+              <Select
+                size="sm"
+                onChange={(e) => setSelectedPlatform(e.target.value)}
+              >
+                <option value="all">All Platforms</option>
+                <option value="ps4">Playstation 4</option>
+                <option value="ps5">Playstation 5</option>
+                <option value="switch">Nintendo Switch</option>
+                <option value="xbone">Xbox One</option>
+                <option value="xbseries">Xbox Series</option>
+              </Select>
+              <Select
+                size="sm"
+                onChange={(e) => setSelectedPhysical(e.target.value)}
+              >
+                <option value="all">All Types</option>
+                <option value="phys">Physical Only</option>
+                <option value="digi">Digital Only</option>
+              </Select>
+              <Input
+                value={searchName}
+                onChange={(e) => setSearchName(e.target.value)}
+                placeholder="Search for Deals"
+                size="sm"
+              />
+              <Button
+                size="sm"
+                minWidth="60px"
+                colorScheme="orange"
+                onClick={searchDeals}
+              >
+                Search
+              </Button>
+            </HStack>
             <DealsTable dealsData={dealsData} />
           </>
         )}
-
       </Container>
     </>
   );
