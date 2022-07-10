137a4203508b63ecf06cc07f287178a535926e0b
Luke Kellett <lkellett2@outlook.com>
2022-07-10 18:52:25 +1000

    added filters for platform and layout for other filters in API

--- app/Http/Controllers/DealsDataController.php
+++ app/Http/Controllers/DealsDataController.php
@@ -25,13 +25,55 @@ class DealsDataController extends Controller
         if ($validator->fails()) {
             return response()->json(["result" => false, "message" => $validator->errors()], 400);
         }
-    
-        try {
-            $deals = DealsData::whereNotNull("min_price")->inRandomOrder()->limit(30)->get();
-            
-            // search params here
 
-            return $deals;
+        try {
+            $deals = DealsData::whereNotNull("min_price");
+            // This runs on page load, if searchType == 'all', no other parameters are looked at.
+            switch ($request->searchType) {
+                case 'all':
+                    $deals = $deals->limit(30)->get();
+                    return $deals;
+                case 'deals':
+                // logic for 'deals' goes here
+                case 'new':
+                    $deals = $deals->whereNull('prv_price');
+                    break;
+            }
+            if (isset($request->physicality)) {
+                switch ($request->physicality) {
+                    case 'phys':
+                        // logic for 'physicals' goes here
+                        break;
+                    case 'digi':
+                        // logic for 'digital' goes here
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (isset($request->platform)) {
+                switch ($request->platform) {
+                    case 'ps4':
+                        $deals = $deals->where('platform', 'PS4');
+                        break;
+                    case 'ps5':
+                        $deals = $deals->where('platform', 'PS5');
+                        break;
+                    case 'switch':
+                        $deals = $deals->where('platform', 'Nintendo Switch');
+                        break;
+                    case 'xbone':
+                        $deals = $deals->where('platform', 'Xbox One');
+                        break;
+                    case 'xbseries':
+                        $deals = $deals->where('platform', 'Xbox Series X');
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return $deals->limit(40)->get(); //TODO: the limit is temperary!
+
         }
         catch (QueryException $ex) {
             //Letting laravel handle the errors instead of these arrays might be the better idea.
